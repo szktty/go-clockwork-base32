f9579cee3762f3320358ef9ba05cdb8030809f73
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2020-07-16 20:30:44 +0900

    'X' が抜けていた

--- base32.go
+++ base32.go
@@ -63,7 +63,8 @@ func Decode(data []byte) ([]byte, error) {
 var encodeSymbols = [32]byte{
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
-	'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'Z',
+	'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X',
+	'Y', 'Z',
 }
 
 var decodeSymbols = [256]int8{
